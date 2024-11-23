8a9d3f684a9b999ea1e4522d8a7c6434eacd22c4
E M <katya-m-2008@mail.ru>
2024-11-24 01:19:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/log_in/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.log_in;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private final String rightpsw = "password777";
+    private EditText psw_field;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn = findViewById(R.id.button);
+        psw_field = findViewById(R.id.Password);
+
+        int duration = Toast.LENGTH_SHORT;
+
+        btn.setOnClickListener(v ->{
+            String psw = psw_field.getText().toString();
+            Toast toast;
+            if (psw.equals(rightpsw)){
+                toast = Toast.makeText(this, "Welcome", duration);
+            } else{
+                toast = Toast.makeText(this, "Invalid password", duration);
+            }
+            toast.show();
+        });
+
+    }
+}
